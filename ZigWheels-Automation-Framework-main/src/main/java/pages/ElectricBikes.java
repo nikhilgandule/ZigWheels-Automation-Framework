2ba6b1296e856988fd72cc4ff2f38cf1925422ca
Mishra <2488303@cognizant.com>
2026-05-29 10:29:01 +0530

    Added Files

---
+++ ZigWheels-Automation-Framework-main/src/main/java/pages/ElectricBikes.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class ElectricBikes {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	Actions action;
+	
+	public ElectricBikes(WebDriver driver) {
+		this.driver=driver;
+		this.wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		this.action=new Actions(driver);
+	}	
+	
+	public void gotoPage() {
+		WebElement moreBtn=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'MORE')]")));
+		action.moveToElement(moreBtn).perform();	
+		
+		WebElement eBike=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-track-label='nav-electric-vehicles']")));
+		eBike.click();
+	}
+	
+	public void searchEVBike() {
+		
+		WebElement Searchbtn=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class,'c-p')]//strong[@class='block']")));
+		Searchbtn.click();
+		
+		WebElement selectBike=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='modal-content']//span[@class='icn i-b ev-sprite']")));
+		selectBike.click();
+	}
+	
+}
